e3cc10567f4beb292051de8fa4a810d0170d195c
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-03 20:33:28 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,41 +1,40 @@
 const carosel = document.querySelector(".carosel");
-const arrowicons = document.querySelectorAll(".wrapper i " );
+const arrowicons = document.querySelectorAll(".wrapper i ");
 const firstImg = document.querySelectorAll(".carosel img ")[0];
-let firstimgwidth= firstImg.clientWidth + 14
+let firstimgwidth = firstImg.clientWidth + 14;
 
 let isDragStart = false;
 let prevpagex;
 let preScroolLeft;
 
-arrowicons.forEach(icon=>{
-    icon.addEventListener('click',()=>{
-        if(icon.id=='left'){
-            console.log('hello')
-            carosel.scrollLeft-=firstimgwidth
-        }else{
-            carosel.scrollLeft+=firstimgwidth
-        }
-    })
-})
+arrowicons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    if (icon.id == "left") {
+      console.log("hello");
+      carosel.scrollLeft -= firstimgwidth;
+    } else {
+      carosel.scrollLeft += firstimgwidth;
+    }
+  });
+});
 
 const dragestart = (e) => {
   isDragStart = true;
-  prevpagex= e.pageX
-  preScroolLeft= carosel.scrollLeft;
+  prevpagex = e.pageX;
+  preScroolLeft = carosel.scrollLeft;
 };
 const draggering = (e) => {
   if (!isDragStart) return;
   e.preventDefault();
- carosel.classList.add('test')
- console.log('worrrrrrrrrrrrrrrrrrrrrk')
+  carosel.classList.add("test");
+  console.log("worrrrrrrrrrrrrrrrrrrrrk");
   let possisiondiff = e.pageX - prevpagex;
   carosel.scrollLeft = preScroolLeft - possisiondiff;
 };
 
-const drageend=()=>{
-    isDragStart = false;
-   
-}
+const drageend = () => {
+  isDragStart = false;
+};
 
 carosel.addEventListener("mousedown", dragestart);
 carosel.addEventListener("touchstart", dragestart);
