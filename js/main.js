f0f3e4c1c30d97222e72a3546f184ce81de26188
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-03 19:52:56 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 const carosel = document.querySelector(".carosel");
+const arrowicons = document.querySelectorAll(".wrapper i " );
+const firstImg = document.querySelectorAll(".carosel img ")[0];
+let firstimgwidth= firstImg.clientWidth + 14
 
 let isDragStart = false;
 let prevpagex;
 let preScroolLeft;
+
+arrowicons.forEach(icon=>{
+    icon.addEventListener('click',()=>{
+        if(icon.id=='left'){
+            console.log('hello')
+            carosel.scrollLeft-=firstimgwidth
+        }else{
+            carosel.scrollLeft+=firstimgwidth
+        }
+    })
+})
 
 const dragestart = (e) => {
   isDragStart = true;
