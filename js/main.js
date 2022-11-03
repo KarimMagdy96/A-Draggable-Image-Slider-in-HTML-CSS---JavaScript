3e9adde3c038be59de80cc3dbdcf6d0fd1f16dd1
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-03 18:08:27 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log('heloooooooooooo')
+const carosel = document.querySelector(".carosel");
+
+let isDragStart = false;
+
+const dragestart = () => {
+  isDragStart = true;
+};
+const draggering = (e) => {
+  if (!isDragStart) return;
+  e.preventDefault();
+  carosel.scrollLeft = e.pageX;
+};
+
+carosel.addEventListener("mousedown", dragestart);
+carosel.addEventListener("mousemove", draggering);
