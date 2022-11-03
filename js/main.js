68d38254aa55ffc72ffcfd62f13e10284958584b
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-03 21:56:18 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const carosel = document.querySelector(".carosel");
 const arrowicons = document.querySelectorAll(".wrapper i ");
 const firstImg = document.querySelectorAll(".carosel img ")[0];
-let firstimgwidth = firstImg.clientHeight +60;
+let firstimgwidth = firstImg.clientHeight +46;
 console.log(firstimgwidth);
 
 let isDragStart = false;
